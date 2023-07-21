56cd3a883b22c4ef7bf0797dd25acf21f438dfeb
MBG1337x <108623200+MBG1337@users.noreply.github.com>
2023-07-21 16:47:15 +0530

    Update CodeUtil.php

--- HubSystem/src/HS/utils/CodeUtil.php
+++ HubSystem/src/HS/utils/CodeUtil.php
@@ -17,9 +17,9 @@ class CodeUtil {
 
 	public static function PlaySound(Player $player, string $sound, $volume = 1, $pitch = 1) {
 		$packet = new PlaySoundPacket();
-		$packet->x = $player->getPosition()->getX();
-		$packet->y = $player->getPosition()->getY();
-		$packet->z = $player->getPosition()->getZ();
+		$packet->x = $player->getPosition()->getFloorX();
+		$packet->y = $player->getPosition()->getFloorY();
+		$packet->z = $player->getPosition()->getFloorZ();
 		$packet->soundName = $sound;
 		$packet->volume = 1;
 		$packet->pitch = 1;
